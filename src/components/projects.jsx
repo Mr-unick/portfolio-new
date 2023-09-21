7b786757de245526a999594625c9958168d3000d
Nikhil <nikhillende9121@gmail.com>
2023-09-22 01:18:33 +0530

    v

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -34,7 +34,7 @@ const data=projects.filter(project=>project.technologies.includes(keyword))
               <img
                 src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
                 alt="Laptop"
-                className="h-[200px] w-full rounded-t-md object-cover" 
+                className="h-[200px] w-full  object-cover rounded-2xl" 
               />
               <div class="relative  h-10 w-28 p-2 px-5 rounded-full bg-white text-black -top-6 left-[85px] flex justify-center items-center" > {project.type} </div>
               <div className="p-4" data-aos="fade-up" >
