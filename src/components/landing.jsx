7b6575e822ba1f496a316d003358628d1c6840b1
Nikhil <nikhillende9121@gmail.com>
2023-09-22 01:33:42 +0530

    v

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -8,11 +8,11 @@ import Typewriter from "typewriter-effect";
  const Landing = (props) => {
   
   return(
-    <div id="landing-page" class=" flex justify-center items-center flex-col rounded-b-8xl  w-full bg-gray-100  ">
-      <div id="top" class="flex justify-center items-center flex-col ">
+    <div id="landing-page" class=" flex justify-center items-center flex-col rounded-b-8xl  w-full bg-gray-100 h-[800px] ">
+      <div id="top" class="flex justify-center items-center flex-col h-96 p-10 ">
     
-              <span class="animate__animated animate__backInLeft text-2xl font-extrabold md:text-4xl">HEY, I'M NIKHIL LENDE</span>
-              <div class="animate__animated animate__backInRight px-4 text-3xl text-blue-700 font-bold mt-2">
+              <span class="animate__animated animate__backInLeft text-3xl  font-extrabold md:text-4xl">HEY, I'M NIKHIL LENDE</span>
+              <div class="animate__animated animate__backInRight  text-2xl md:text-3xl text-blue-700 font-bold mt-2 w-full flex justify-center items-center">
                           <Typewriter
                           options={{
                             loop: true, // Enable infinite loop
@@ -35,7 +35,14 @@ import Typewriter from "typewriter-effect";
                         }}
                         />
               </div>
-              <p class="text-gray-500 mt-3 animate__animated animate__fadeInUp flex flex-wrap  w-[340px] p-2">A dedicated full-stack developer with proficient knowledge in the MERN stack.</p>
+              <p class="text-gray-500 mt-3 animate__animated animate__fadeInUp flex flex-wrap  w-[340px] p-2">
+                A dedicated full-stack developer with proficient knowledge in the MERN stack.</p>
+<div className="text-white flex ">
+
+                  <button class="px-5 py-2 flex justify-center items-center rounded-full bg-blue-700 m-4">
+                  Contact Me</button>
+</div>
+                
               </div>
     
      <div id="image" class="w-full  flex justify-center items-center  ">
