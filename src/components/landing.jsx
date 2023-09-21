21db3d09ad89f87d523171e48770d1b2ae6796c7
Nikhil <nikhillende9121@gmail.com>
2023-09-22 01:02:33 +0530

    v

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,19 +2,44 @@ import React from 'react'
 import "./style.css"
 import Text from './text';
 import 'animate.css';
+import Typewriter from "typewriter-effect";
+
 
  const Landing = (props) => {
   
   return(
-    <div id="landing-page" class=" flex justify-center items-center flex-col rounded-b-8xl mt-8 w-full">
-      <div id="top" class="flex justify-center items-center flex-col h-1/2 ">
+    <div id="landing-page" class=" flex justify-center items-center flex-col rounded-b-8xl  w-full bg-gray-100  ">
+      <div id="top" class="flex justify-center items-center flex-col   ">
     
-      <span class="animate__animated animate__backInLeft text-4xl font-extrabold">HEY, I'M NIKHIL LENDE</span>
-     <div class="animate__animated animate__backInRight px-4"> <Text /></div>
-     <span class="text-gray-500 mt-5 animate__animated animate__fadeInUp flex flex-wrap">A dedicated full-stack developer with proficient knowledge in the MERN stack.</span></div>
+              <span class="animate__animated animate__backInLeft text-2xl font-extrabold md:text-4xl">HEY, I'M NIKHIL LENDE</span>
+              <div class="animate__animated animate__backInRight px-4 text-3xl text-blue-700 font-bold mt-2">
+                          <Typewriter
+                          options={{
+                            loop: true, // Enable infinite loop
+                          }}
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("REACT JS DEVLOPER")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("ANDROID DEVLOPER")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("FULLSTACK DEVLOPER")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("WEB DESIGNER")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .start();
+                        }}
+                        />
+              </div>
+              <p class="text-gray-500 mt-3 animate__animated animate__fadeInUp flex flex-wrap  w-[340px] p-2">A dedicated full-stack developer with proficient knowledge in the MERN stack.</p>
+              </div>
     
      <div id="image" class="w-full  flex justify-center items-center  ">
-     <img src="images/bg.png" class="  w-[500px] h-[400px]  " />
+         <img src="images/bg.png" class="  w-[600px]  h-[300px] md:h-[400px] " />
      </div>
     </div>
    )
