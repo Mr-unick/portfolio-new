78ae56dfeb552316a338ea0c82451e65ba440b70
Nikhil <nikhillende9121@gmail.com>
2023-10-04 13:00:17 +0530

    v

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -13,14 +13,14 @@ export const Skills = (props) => {
     Skillsdata.map((skill)=>{
         return(
             <div class="flex justify-center items-center flex-col w-screen   p-4   m-2 " data-aos="fade-up">
-                <h1 class="text-2xl  font-extrabold p-2  w-screen flex justify-center items-center text-gray-500" >{skill.categoryName}</h1>
+                <h1 class="text-2xl  font-extrabold p-2  w-screen flex justify-center items-center text-gray-500 dark:text-gray-400" >{skill.categoryName}</h1>
                 <div class="flex flex-wrap justify-center items-center" >
                 {
                     skill.skills.map(skill=>{
                         return(
-                            <div id="card" class="flex justify-center items-center flex-col md:w-36 w-32   m-2 " >
+                            <div id="card" class="flex justify-center items-center flex-col md:w-36 w-32   dark:text-gray-400 m-2 " >
                             <img src={skill.skillData} class=" md:h-[70px] md:w-[70px] w-[50px] h-[50px] m-2" />
-                            <span class="font-bold text-gray-500 mt-2 w-full flex justify-center items-center">{skill.skillName}</span>
+                            <span class="font-bold text-gray-500 mt-2 w-full flex justify-center  dark:text-gray-400 items-center">{skill.skillName}</span>
                             </div>
                         )
                     })
