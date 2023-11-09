24c4074f92bdb08c44a0108343a5dd32feee3d66
Nikhil <nikhillende9121@gmail.com>
2023-11-09 14:48:58 +0530

    v

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -16,7 +16,7 @@ import { Background, Parallax } from 'react-parallax';
      
       >
        
-    <div  id='landing' class=" flex justify-center items-center md:items-center flex-col dark:bg-black dark:bg-opacity-50 h-screen w-screen text-start px-6">
+    <div  id='landing' class=" flex justify-center items-center  flex-col dark:bg-black dark:bg-opacity-50 h-screen w-screen text-start px-6">
     <p class="animate__animated animate__backInLeft text-4xl font-bold md:text-5xl dark:text-white text-white md:my-2 font-[Teko', sans-serif]  flex flex-wrap">HEY, I'M NIKHIL LENDE</p>
     <div class="animate__animated animate__backInRight  text-2xl md:text-3xl text-gray-200 font-extrabold mt-2 w-full flex  items-start md:justify-center   ">
                 <Typewriter
@@ -41,11 +41,11 @@ import { Background, Parallax } from 'react-parallax';
               }}
               />
     </div>
-    <p class="text-gray-200 mt-3 animate__animated animate__fadeInUp flex flex-wrap   dark:text-gray-400 md:text-center text-start md:my-4 w-full">
+    <p class="text-gray-200 mt-3 animate__animated animate__fadeInUp flex flex-wrap   dark:text-gray-400  md:justify-center justify-start md:items-center text-start md:text-center items-start  md:my-4 w-full md:[w-340px]">
       A dedicated full-stack developer with proficient knowledge in the MERN stack.</p>
-    <div className="text-white flex iteams-start md:items-center  w-full">
+    <div className="text-white flex md:justify-center md:items-center  items-start justify-start  w-full">
 
-        <button id='Downloadbutton' class="px-5 py-1 flex justify-center items-start md:items-center rounded-full  mt-4 md:m-4 font-bold  border-2 border-white  ">
+        <button id='Downloadbutton' class="px-5 py-1 flex md:items-center text-center items-start  rounded-full  mt-4 md:m-4 font-bold  border-2 border-white  ">
         <a class="m-1" href='pdf/NikhilLende.pdf' download="NikhilLende.pdf">Get My Resume</a></button>
        </div>
 
