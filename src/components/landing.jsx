d088c20ad6686ce5a8a3f06feb2a4a2926af1226
Nikhil <nikhillende9121@gmail.com>
2023-11-09 14:32:59 +0530

    v

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,28 +4,20 @@ import "./style.css"
 import 'animate.css';
 import Typewriter from "typewriter-effect";
 import { Socials } from './footer';
-import { Parallax } from 'react-parallax';
+import { Background, Parallax } from 'react-parallax';
 
  const Landing = (props) => {
   
   return(
     <Parallax
-       
-        bgImage="./bg.svg"
-        bgImageAlt="Background Image"
-        strength={400}
-        style={{
-          height: '100vh',
-          backgroundSize: 'contain', 
-          backgroundAttachment: 'fixed',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+    strength={600}
+     className="md:h-screen h-[800px] w-screen flex justify-center items-center " 
+     bgImage='./gggg.svg'
+     
       >
-    
-    <div  class=" flex justify-center items-center md:items-center flex-col dark:bg-black dark:bg-opacity-50 h-screen w-screen">
-    <p class="animate__animated animate__backInLeft text-4xl font-bold md:text-5xl dark:text-white text-white md:my-2 font-[Teko', sans-serif]">HEY, I'M NIKHIL LENDE</p>
+       
+    <div  id='landing' class=" flex justify-center items-center md:items-center flex-col dark:bg-black dark:bg-opacity-50 h-screen w-screen text-start px-6">
+    <p class="animate__animated animate__backInLeft text-4xl font-bold md:text-5xl dark:text-white text-white md:my-2 font-[Teko', sans-serif]  flex flex-wrap">HEY, I'M NIKHIL LENDE</p>
     <div class="animate__animated animate__backInRight  text-2xl md:text-3xl text-gray-200 font-extrabold mt-2 w-full flex  items-start md:justify-center   ">
                 <Typewriter
                 options={{
@@ -49,11 +41,11 @@ import { Parallax } from 'react-parallax';
               }}
               />
     </div>
-    <p class="text-gray-200 mt-3 animate__animated animate__fadeInUp flex flex-wrap  w-[340px] dark:text-gray-400 text-center md:my-4">
+    <p class="text-gray-200 mt-3 animate__animated animate__fadeInUp flex flex-wrap  w-[340px] dark:text-gray-400 md:text-center text-start md:my-4 w-full">
       A dedicated full-stack developer with proficient knowledge in the MERN stack.</p>
-    <div className="text-white flex ">
+    <div className="text-white flex iteams-start md:items-center  w-full">
 
-        <button id='Downloadbutton' class="px-5 py-1 flex justify-center items-center rounded-full  mt-4 md:m-4 font-bold  border-2 border-white  ">
+        <button id='Downloadbutton' class="px-5 py-1 flex justify-center items-start md:items-center rounded-full  mt-4 md:m-4 font-bold  border-2 border-white  ">
         <a class="m-1" href='pdf/NikhilLende.pdf' download="NikhilLende.pdf">Get My Resume</a></button>
        </div>
 
